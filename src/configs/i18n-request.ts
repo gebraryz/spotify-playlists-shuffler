8a5d8239d59be1eb11952fa3c199d2d611be8335
gebraryz <gebraryz@gmail.com>
2025-01-01 13:36:04 +0100

    Update dependencies and refactor i18n request configuration

--- src/configs/i18n-request.ts
+++ src/configs/i18n-request.ts
@@ -3,12 +3,15 @@ import { getRequestConfig } from 'next-intl/server';
 
 import { i18nRouting } from './i18n-routing';
 
-export default getRequestConfig(async ({ locale }) => {
+export default getRequestConfig(async ({ requestLocale }) => {
+  const locale = await requestLocale;
+
   if (!i18nRouting.locales.includes(locale as any)) {
     notFound();
   }
 
   return {
+    locale,
     messages: (await import(`../translations/${locale}.json`)).default,
   };
 });
